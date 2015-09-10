542e633a7a9ae703bf1ad3e4b9600cd49fb16f82
ins1gn1a <ins1gn1a@users.noreply.github.com>
2015-09-10 10:11:58 +0100

    Update cardscan4linux.py

--- cardscan4linux.py
+++ cardscan4linux.py
@@ -40,8 +40,6 @@ os.system('find %s -maxdepth %s -type f \( -name "*.txt"%s \) %s %s > /tmp/cards
 # Count how many entries in the list file
 file_lines = sum(1 for count_lines in open('/tmp/cardscan4linux.list'))
 
-print file_lines
-
 # Regex to filter card numbers
 regexAmex = re.compile("([^0-9-]|^)(3(4[0-9]{2}|7[0-9]{2})( |-|)[0-9]{6}( |-|)[0-9]{5})([^0-9-]|$)") #16 Digit AMEX
 regexVisa = re.compile("([^0-9-]|^)(4[0-9]{3}( |-|)([0-9]{4})( |-|)([0-9]{4})( |-|)([0-9]{4}))([^0-9-]|$)")
